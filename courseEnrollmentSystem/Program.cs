3ee481a017554cc0e1c0df8d133978ad4b0d5768
Azzak <azzaalmahrooqi79@gmail.com>
2024-09-23 10:12:30 +0400

    Declare dictionary to store courses and enrolled students ,and Waitlist to store students who are waiting to enroll in a full course.

--- courseEnrollmentSystem/Program.cs
+++ courseEnrollmentSystem/Program.cs
@@ -2,9 +2,12 @@
 {
     internal class Program
     {
+        static Dictionary<string, HashSet<string>> courses = new Dictionary<string, HashSet<string>>();// Dictionary to store courses and enrolled students
+        static List<(string studentName, string courseCode)> WaitList = new List<(string, string)>();// Waitlist to store students who are waiting to enroll in a full course
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
         }
     }
 }
